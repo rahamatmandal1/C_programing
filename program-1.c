7ea743bb90dee028b6c58452a86bb8840f47f218
rahamatmandal1 <rahamatmandal1@eng.rizvi.edu.in>
2025-10-11 11:39:27 +0530

    Add files via upload

---
+++ program-1.c
@@ -0,0 +1,21 @@
+/*Name : Mandal Rahamatulla Faruk
+UIN : 251M008
+Class : F.E Mechanical
+DIV : F */
+#include<stdio.h>
+
+int main(){
+    char Name[]= "Mandal Rahamatulla Faruk";
+    int Age = 18;
+    char Class[]= "F.E Mechanical";
+    char DIV[]= "F";
+    char UIN[]= "251M008";
+    
+    printf("Name\t: %s\n", Name);
+      printf("Age\t: %d\n", Age);  
+      printf("Class\t: %s\n", Class);  
+      printf("DIV\t: %s\n", DIV);
+      printf("UIN\t: %s\n", UIN);
+      
+      return 0;
+}
