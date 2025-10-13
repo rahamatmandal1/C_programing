934392dd30251a72315495abc1d9adccaccd282b
rahamatmandal1 <rahamatmandal1@eng.rizvi.edu.in>
2025-10-13 09:27:48 +0530

    Update Experiment2a.c

--- Experiment2a.c
+++ Experiment2a.c
@@ -6,12 +6,10 @@ DIV : F */
 
 int main() {
     int num;
-    
-    // Input from user
+
     printf("Enter a number: ");
     scanf("%d", &num);
     
-    // Check even or odd
     if (num % 2 == 0)
         printf("%d is even.\n", num);
     else
