61fed562fd05d2e5ac42b4959fdc14035898727e
rahamatmandal1 <rahamatmandal1@eng.rizvi.edu.in>
2025-11-14 10:41:11 +0530

    Create Experiment 7b.c

---
+++ Experiment 7b.c
@@ -0,0 +1,48 @@
+/*Name: Mandal Rahamatulla Faruk
+UIN: 251M008
+class: F.E Mechanical
+DIV: F */
+
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int a[rows][cols], b[rows][cols], sum[rows][cols];
+
+    printf("Enter elements of Matrix A:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter elements of Matrix B:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    printf("\nSum of Two Matrices:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
